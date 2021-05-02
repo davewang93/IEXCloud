81f1bf7950d60a44cffffa589695a5a8ca91124b
davewang <dwang07@gmail.com>
2021-05-01 20:15:11 -0400

    corrected cache

--- iexcryptoprices.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-set root=C:\ProgramData\Anaconda3\
-call %root%\Scripts\activate.bat %root%
-C:\ProgramData\Anaconda3\python.exe "D:\OneDrive\David\src\IEXCloud\IEXCryptoUpdater.py"
-pause
